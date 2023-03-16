907c1830eb0164fe04ebc2e98adb3b4f083821b5
Danilo Rocha <dsrocha@stefanini.com>
2023-03-16 11:15:25 -0300

    Fade fast

--- src/pages/fast/index.tsx
+++ src/pages/fast/index.tsx
@@ -130,169 +130,171 @@ export default function FastSchedule({ barbers, user }: HaircutsProps) {
       <Head>
         <title>Agende seu corte - Rocha's client</title>
       </Head>
+
       <Flex direction={isMobile ? "column" : "row"} background="barber.900" minH="100vh" align="center" justify='center'>
+        <FadeIn>
+          <Flex align="center" justify='center' w='100%' p={isMobile ? 5 : 10} direction={isMobile ? "column" : "row"}>
 
-        <Flex align="center" justify='center' w='100%' p={isMobile ? 5 : 10} direction={isMobile ? "column" : "row"}>
-
-          <Flex direction="column" align='center' justify='center' w="100%">
-            <Heading fontSize={isMobile ? "3xl" : "5xl"} color="#EF871D" fontWeight="bold" textAlign='center' mb={isMobile ? 5 : 10}>Agendamento rápido
-            </Heading>
-            <Center mb={isMobile ? 5 : 10}>
-              <Image
-                src={logoImg}
-                quality={100}
-                width={isMobile ? 150 : 450}
-                objectFit="fill"
-                alt="Logo Rocha's Client"
-              />
-            </Center>
-          </Flex>
-
-          <Flex
-            borderLeft={isMobile ? "0px solid #fff" : "5px solid #fff"}
-            borderTop={isMobile ? "4px solid #fff" : "0px solid #fff"}
-            direction="column"
-            w="100%" ml={isMobile ? 0 : 10} p={isMobile ? 0 : 10} >
-
-            <Flex direction="column" w="100%">
-              <Text color="white" mb={1} mt={isMobile ? 5 : 0} fontSize="xl" fontWeight="bold">Informe seu nome:</Text>
-              <Input
-                background="#171922"
-                _hover={{ bg: "#1b1c29" }}
-                color="white"
-                variant="filled"
-                size="lg"
-                placeholder="Digite seu nome:"
-                type="text"
-                mb={3}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
+            <Flex direction="column" align='center' justify='center' w="100%">
+              <Heading fontSize={isMobile ? "3xl" : "5xl"} color="#EF871D" fontWeight="bold" textAlign='center' mb={isMobile ? 5 : 10}>Agendamento rápido
+              </Heading>
+              <Center mb={isMobile ? 5 : 10}>
+                <Image
+                  src={logoImg}
+                  quality={100}
+                  width={isMobile ? 150 : 450}
+                  objectFit="fill"
+                  alt="Logo Rocha's Client"
+                />
+              </Center>
             </Flex>
 
-            {name &&
-              <FadeIn>
-                <Flex direction="column" w="100%">
-                  <Text color="white" mb={1} fontSize="xl" fontWeight="bold">Escolha o barbeiro:</Text>
-                  <Select
-                    color="white"
-                    w="100%"
-                    bg="gray.900"
-                    size="lg"
-                    mb={3}
-                    onChange={(e) => handleChangeSelectBarber(e.target.value)}
-                    defaultValue=""
-                  >
-                    <option disabled value="">Selecione um barbeiro</option>
-                    {barbers?.map(item => {
-                      return (
-                        <option style={{ background: "#1b1c29" }} key={item?.id} value={item?.id}>{item?.barber_name}</option>
-                      )
-                    })}
-                  </Select>
-                </Flex>
-              </FadeIn>
+            <Flex
+              borderLeft={isMobile ? "0px solid #fff" : "5px solid #fff"}
+              borderTop={isMobile ? "4px solid #fff" : "0px solid #fff"}
+              direction="column"
+              w="100%" ml={isMobile ? 0 : 10} p={isMobile ? 0 : 10} >
 
-            }
+              <Flex direction="column" w="100%">
+                <Text color="white" mb={1} mt={isMobile ? 5 : 0} fontSize="xl" fontWeight="bold">Informe seu nome:</Text>
+                <Input
+                  background="#171922"
+                  _hover={{ bg: "#1b1c29" }}
+                  color="white"
+                  variant="filled"
+                  size="lg"
+                  placeholder="Digite seu nome:"
+                  type="text"
+                  mb={3}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Flex>
 
-            {barberSelected &&
-              <FadeIn>
-                <Flex direction="column" w="100%">
-                  <Text color="white" mb={1} fontSize="xl" fontWeight="bold">Escolha o corte:</Text>
-                  <Select
-                    color="white"
-                    w="100%"
-                    bg="gray.900"
-                    size="lg"
-                    mb={3}
-                    onChange={(e) => handleChangeSelectHaircut(e.target.value)}
-                    defaultValue=""
-                  >
-                    <option disabled value="">Selecione um corte</option>
-                    {barberSelected?.haircuts.map(item => {
-                      if (item.status) {
+              {name &&
+                <FadeIn>
+                  <Flex direction="column" w="100%">
+                    <Text color="white" mb={1} fontSize="xl" fontWeight="bold">Escolha o barbeiro:</Text>
+                    <Select
+                      color="white"
+                      w="100%"
+                      bg="gray.900"
+                      size="lg"
+                      mb={3}
+                      onChange={(e) => handleChangeSelectBarber(e.target.value)}
+                      defaultValue=""
+                    >
+                      <option disabled value="">Selecione um barbeiro</option>
+                      {barbers?.map(item => {
                         return (
-                          <option style={{ background: "#1b1c29" }} key={item?.id} value={item?.id}>{item?.name}</option>
+                          <option style={{ background: "#1b1c29" }} key={item?.id} value={item?.id}>{item?.barber_name}</option>
                         )
-                      }
-                      return null
-                    })}
-                  </Select>
-                </Flex>
-              </FadeIn>
-            }
+                      })}
+                    </Select>
+                  </Flex>
+                </FadeIn>
 
-            {haircutSelected &&
-              <FadeIn>
-                <Flex align='end' justify='center' w="100%" mb={3} mt={3}>
-                  {!availableTime ?
-                    <Button onClick={handleClickItem} w="100%" h="45px" bg='#fff' isLoading={showSpinner}>
-                      {!date ? "Escolha o dia" : `Corte dia: ${date.getDate()}/${date.getMonth() + 1}`}
-                      <ModalCalendary
-                        isOpen={isOpen}
-                        onClose={() => {
-                          setShowSpinner(true)
-                          onClose()
-                        }}
-                        onOpen={onOpen}
-                        setDate={setDate}
-                        date={date}
-                        setDateSelected={setDateSelected}
-                      />
-                    </Button> :
-                    <FadeIn>
-                      <Flex direction='column' w='100%'>
-                        <Text color="white" mb={1} fontSize="xl" fontWeight="bold">Escolha o horário:</Text>
-                        <Flex direction="row" align='center' justify='space-between' >
-                          <SelectTime availableTime={validatedAvaliableTime(availableTime)} timeUsed={timeUsed} initialAvailableTime={validatedAvaliableTime(initialAvailableTime)} timesAlreadyUsed={validatedAvaliableTime(timesUsed)} setTimeToUsed={setTimeToUsed} setOpenResume={setOpenResume} />
-                          <Button onClick={handleClickItem} h="40px" w={isMobileSmall ? "30%" : (isMobile ? "50%" : "60%")} bg='white' p={1} isLoading={!showSpinner}>
-                            {!date ? "Escolha o dia" : (isMobileSmall ? `Dia: ${date.getDate()}/${date.getMonth() + 1}` :
-                              `Corte dia: ${date.getDate()}/${date.getMonth() + 1}`)}
-                            {!openResume &&
-                              <ModalCalendary
-                                isOpen={isOpen}
-                                onClose={onClose}
-                                onOpen={onOpen}
-                                setDate={setDate}
-                                date={date}
-                                setDateSelected={setDateSelected}
-                              />}
-                          </Button>
+              }
+
+              {barberSelected &&
+                <FadeIn>
+                  <Flex direction="column" w="100%">
+                    <Text color="white" mb={1} fontSize="xl" fontWeight="bold">Escolha o corte:</Text>
+                    <Select
+                      color="white"
+                      w="100%"
+                      bg="gray.900"
+                      size="lg"
+                      mb={3}
+                      onChange={(e) => handleChangeSelectHaircut(e.target.value)}
+                      defaultValue=""
+                    >
+                      <option disabled value="">Selecione um corte</option>
+                      {barberSelected?.haircuts.map(item => {
+                        if (item.status) {
+                          return (
+                            <option style={{ background: "#1b1c29" }} key={item?.id} value={item?.id}>{item?.name}</option>
+                          )
+                        }
+                        return null
+                      })}
+                    </Select>
+                  </Flex>
+                </FadeIn>
+              }
+
+              {haircutSelected &&
+                <FadeIn>
+                  <Flex align='end' justify='center' w="100%" mb={3} mt={3}>
+                    {!availableTime ?
+                      <Button onClick={handleClickItem} w="100%" h="45px" bg='#fff' isLoading={showSpinner}>
+                        {!date ? "Escolha o dia" : `Corte dia: ${date.getDate()}/${date.getMonth() + 1}`}
+                        <ModalCalendary
+                          isOpen={isOpen}
+                          onClose={() => {
+                            setShowSpinner(true)
+                            onClose()
+                          }}
+                          onOpen={onOpen}
+                          setDate={setDate}
+                          date={date}
+                          setDateSelected={setDateSelected}
+                        />
+                      </Button> :
+                      <FadeIn>
+                        <Flex direction='column' w='100%'>
+                          <Text color="white" mb={1} fontSize="xl" fontWeight="bold">Escolha o horário:</Text>
+                          <Flex direction="row" align='center' justify='space-between' >
+                            <SelectTime availableTime={validatedAvaliableTime(availableTime)} timeUsed={timeUsed} initialAvailableTime={validatedAvaliableTime(initialAvailableTime)} timesAlreadyUsed={validatedAvaliableTime(timesUsed)} setTimeToUsed={setTimeToUsed} setOpenResume={setOpenResume} />
+                            <Button onClick={handleClickItem} h="40px" w={isMobileSmall ? "30%" : (isMobile ? "50%" : "60%")} bg='white' p={1} isLoading={!showSpinner}>
+                              {!date ? "Escolha o dia" : (isMobileSmall ? `Dia: ${date.getDate()}/${date.getMonth() + 1}` :
+                                `Corte dia: ${date.getDate()}/${date.getMonth() + 1}`)}
+                              {!openResume &&
+                                <ModalCalendary
+                                  isOpen={isOpen}
+                                  onClose={onClose}
+                                  onOpen={onOpen}
+                                  setDate={setDate}
+                                  date={date}
+                                  setDateSelected={setDateSelected}
+                                />}
+                            </Button>
+                          </Flex>
                         </Flex>
-                      </Flex>
-                    </FadeIn>
-                  }
-                </Flex>
-              </FadeIn>
-            }
+                      </FadeIn>
+                    }
+                  </Flex>
+                </FadeIn>
+              }
 
-            {openResume &&
-              <ModalResume
-                isOpen={isOpen}
-                onClose={onClose}
-                onOpen={onOpen}
-                openResume={openResume}
-                data={dataResume}
-                setOpenResume={setOpenResume}
-              />
-            }
+              {openResume &&
+                <ModalResume
+                  isOpen={isOpen}
+                  onClose={onClose}
+                  onOpen={onOpen}
+                  openResume={openResume}
+                  data={dataResume}
+                  setOpenResume={setOpenResume}
+                />
+              }
 
-            <Button
-              background="button.cta"
-              mb={6}
-              color="gray.900"
-              h="45px"
-              size="lg"
-              _hover={{ bg: "#ffb13e" }}
-              isLoading={loader}
-              isDisabled={!timeToUsed}
-              onClick={handleRegister}
-            >
-              Agendar
-            </Button>
+              <Button
+                background="button.cta"
+                mb={6}
+                color="gray.900"
+                h="45px"
+                size="lg"
+                _hover={{ bg: "#ffb13e" }}
+                isLoading={loader}
+                isDisabled={!timeToUsed}
+                onClick={handleRegister}
+              >
+                Agendar
+              </Button>
+            </Flex>
+
           </Flex>
-
-        </Flex>
+        </FadeIn>
       </Flex>
     </>
   )
